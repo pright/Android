2e9860eeb7b724482a6ebc91cf9c165fefaab452
pright <pright.xj@gmail.com>
2017-04-01 18:02:40 +0800

    Use Aliyun maven repo.

--- abs_with_gradle/gradle.mk
+++ abs_with_gradle/gradle.mk
@@ -4,14 +4,83 @@ GRADLE_USER_HOME := gradle
 
 TMP_DIR := /tmp/gradle_$(LOCAL_MODULE)
 
+
 ifneq ($(filter update_deps, $(MAKECMDGOALS)), )
 
+define INIT_GRADLE
+gradle.projectsLoaded {
+    rootProject.allprojects {
+        buildscript {
+            repositories {
+                def REPOSITORY_URL = 'http://maven.aliyun.com/nexus/content/groups/public/'
+                all { ArtifactRepository repo ->
+                    if (repo instanceof MavenArtifactRepository) {
+                        def url = repo.url.toString()
+                        if (url.startsWith('Central Repository:')
+                            || url.startsWith('https://jcenter.bintray.com/')) {
+                            project.logger.lifecycle "Repository $${repo.url} replaced by $$REPOSITORY_URL."
+                            remove repo
+                        }
+                    }
+                }
+                jcenter {
+                    url REPOSITORY_URL
+                }
+            }
+        }
+        repositories {
+            def REPOSITORY_URL = 'http://maven.aliyun.com/nexus/content/groups/public/'
+            all { ArtifactRepository repo ->
+                if (repo instanceof MavenArtifactRepository) {
+                    def url = repo.url.toString()
+                    if (url.startsWith('Central Repository:')
+                        || url.startsWith('https://jcenter.bintray.com/')) {
+                        project.logger.lifecycle "Repository $${repo.url} replaced by $$REPOSITORY_URL."
+                        remove repo
+                    }
+                }
+            }
+            jcenter {
+                url REPOSITORY_URL
+            }
+        }
+    }
+}
+endef
+
+define GITIGNORE
+/daemon/*
+/native/*
+/caches/*
+!/caches/modules-*/
+/wrapper/dists/**
+!/wrapper/dists/gradle-*/
+!/wrapper/dists/gradle-*/*/
+!/wrapper/dists/gradle-*/*/*.zip
+*.lock
+endef
+
+
+$(GRADLE_USER_HOME)/init.gradle: export INIT_GRADLE:=$(INIT_GRADLE)
+$(GRADLE_USER_HOME)/init.gradle:
+	@cd $(PROJECT_PATH); \
+	echo "$${INIT_GRADLE}" > $@
+
+$(GRADLE_USER_HOME)/.gitignore: export GITIGNORE:=$(GITIGNORE)
+$(GRADLE_USER_HOME)/.gitignore:
+	@cd $(PROJECT_PATH); \
+	echo "$${GITIGNORE}" > $@
+
+.PHONY: prepare
+prepare: $(GRADLE_USER_HOME)/init.gradle $(GRADLE_USER_HOME)/.gitignore
+
 .PHONY: update_deps
-update_deps:
+update_deps: prepare
 	@cd $(PROJECT_PATH); \
 	rm -rf $(TMP_DIR); \
 	mkdir -p $(TMP_DIR); \
-	GRADLE_USER_HOME=$(TMP_DIR) JAVA_HOME=$(JAVA_HOME) ANDROID_HOME=$(ANDROID_HOME) ./gradlew --refresh-dependencies; \
+	cp -a $(GRADLE_USER_HOME)/* $(TMP_DIR); \
+	GRADLE_USER_HOME=$(TMP_DIR) JAVA_HOME=$(JAVA_HOME) ANDROID_HOME=$(ANDROID_HOME) ./gradlew -I $(GRADLE_USER_HOME)/init.gradle --refresh-dependencies; \
 	cp -a $(TMP_DIR)/* $(GRADLE_USER_HOME); \
 	rm -rf $(TMP_DIR);
 
@@ -22,9 +91,9 @@ build_clean:
 	@cd $(PROJECT_PATH); \
 	rm -rf $(TMP_DIR) && \
 	ln -sf $$(pwd)/$(GRADLE_USER_HOME) $(TMP_DIR); \
-	GRADLE_USER_HOME=$(TMP_DIR) JAVA_HOME=$(JAVA_HOME) ANDROID_HOME=$(ANDROID_HOME) ./gradlew --offline clean; \
+	GRADLE_USER_HOME=$(TMP_DIR) JAVA_HOME=$(JAVA_HOME) ANDROID_HOME=$(ANDROID_HOME) ./gradlew -I $(GRADLE_USER_HOME)/init.gradle --offline clean; \
 	rm -rf $(TMP_DIR);
- 
+
 else
 
 ifneq ($(PROJECT_SRC_DIRS), )
@@ -38,7 +107,7 @@ $(PROJECT_PATH)/$(LOCAL_SRC_FILES): $(PROJECT_SRC)
 	@cd $(PROJECT_PATH); \
 	rm -rf $(TMP_DIR) && \
 	ln -sf $$(pwd)/$(GRADLE_USER_HOME) $(TMP_DIR); \
-	GRADLE_USER_HOME=$(TMP_DIR) JAVA_HOME=$(JAVA_HOME) ANDROID_HOME=$(ANDROID_HOME) ./gradlew --offline build; \
+	GRADLE_USER_HOME=$(TMP_DIR) JAVA_HOME=$(JAVA_HOME) ANDROID_HOME=$(ANDROID_HOME) ./gradlew -I $(GRADLE_USER_HOME)/init.gradle --offline build; \
 	rm -rf $(TMP_DIR);
 
 include $(BUILD_PREBUILT)
